4f9794a1ef7b22e01c5a7a73236ea6185f5e8445
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-19 17:32:43 +0530

    Hamburger started.

---
+++ src/main/java/com/stackroute/billsburger/Hamburger.java
@@ -0,0 +1,11 @@
+package com.stackroute.billsburger;
+
+import java.util.List;
+
+public class Hamburger {
+    private int id;
+    private String breadRollType;
+    private String meat;
+    private List<AdditionalItem> additions;
+    private double price;
+}
