29571972a2c3dce8b32961eaca3ee2075c16fec4
praveen <praveenrajjoth@gmail.com>
2019-07-19 17:41:14 +0530

    Order class created

--- src/main/java/com/stackroute/billsburger/Order.java
+++ src/main/java/com/stackroute/billsburger/Order.java
@@ -1,4 +1,29 @@
 package com.stackroute.billsburger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private int orderNumber;
+    private List<Hamburger> hamburgersList = new ArrayList<>();
+
+    public void Order(int orderNo){
+        this.orderNumber = orderNo;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public void addBurger(Hamburger hamburger){
+        hamburgersList.add(hamburger);
+    }
+
+    public double getTotalCost(){
+        return 0;
+    }
 }
