7a54a5835713363c4c7a0349f8d3c36357b73f5d
praveen <praveenrajjoth@gmail.com>
2019-07-19 18:05:55 +0530

    displayMenu method is created in bills burger class

--- src/main/java/com/stackroute/billsburger/BillsBurger.java
+++ src/main/java/com/stackroute/billsburger/BillsBurger.java
@@ -1,4 +1,9 @@
 package com.stackroute.billsburger;
 
 public class BillsBurger {
+    public void displayMenu() {
+        System.out.println("1 -> Hamburger(Hamburger, can have 4 additional items \t Price: 50");
+        System.out.println("2 -> Healthy Burger(Healthy Burger, can have 6 additional items \t 60");
+        System.out.println("3 -> Deluxe Burger(Deluxe Burger, can have 4 additional items \n And also it contains chips and drink \t Price: 70");
+    }
 }
