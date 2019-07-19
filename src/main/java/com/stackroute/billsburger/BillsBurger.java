65e4936ee19228409ff4936707bc21a7aebc2b0e
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-19 18:06:40 +0530

    BillsBurger: Main method created.

--- src/main/java/com/stackroute/billsburger/BillsBurger.java
+++ src/main/java/com/stackroute/billsburger/BillsBurger.java
@@ -1,4 +1,34 @@
 package com.stackroute.billsburger;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BillsBurger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        List<Order> orderList = new ArrayList<>();
+        while (true) {
+            System.out.println("1. New Order\n2. Update Order");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    newOrder();
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Please enter correct choice");
+            }
+        }
+    }
+
+    private static void newOrder() {
+        displayMenu();
+    }
+
+
 }
