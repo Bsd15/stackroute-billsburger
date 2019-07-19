8b81d272e9489fabc01d95b6bee6a65fa22e7b8f
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-19 17:56:21 +0530

    DeluxeBurger completed.

--- src/main/java/com/stackroute/billsburger/DeluxBurger.java
+++ src/main/java/com/stackroute/billsburger/DeluxBurger.java
@@ -1,8 +1,14 @@
 package com.stackroute.billsburger;
 
 public class DeluxBurger extends Hamburger {
-    public DeluxBurger(String meat, double price) {
-        String breadRollType = "";
+    public DeluxBurger(String breadRollType, String meat, double price) {
         super(breadRollType, meat, price);
+        getAdditions().add(new AdditionalItem("Chips", 0));
+        getAdditions().add(new AdditionalItem("Coke", 0));
+    }
+
+    @Override
+    public boolean addItem(AdditionalItem item) {
+        return false;
     }
 }
