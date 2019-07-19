8d3b6f9097172493d91789229cd92217b3b88d04
praveen <praveenrajjoth@gmail.com>
2019-07-19 17:49:49 +0530

    Healthy Burger started

---
+++ src/main/java/com/stackroute/billsburger/HealthyBurger.java
@@ -0,0 +1,7 @@
+package com.stackroute.billsburger;
+
+public class HealthyBurger extends Hamburger {
+    public HealthyBurger(String breadRollType, String meat, double price) {
+        super(breadRollType, meat, price);
+    }
+}
